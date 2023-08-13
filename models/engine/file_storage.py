e0e5ffdef41c976e529fac1528b690c058921cd1
= <=>
2023-08-13 12:08:52 -0700

    model files created

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,52 +1,58 @@
 #!/usr/bin/python3
-"""The storage file is created today"""
+"""storage file created """
 import json
 from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.review import Review
 from models.user import User
-from models.state import State
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
+from models.review import Review
+from models.state import State
 
 
-class FileStorage:
-    """An abstract class of the file is created
-
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
+class FileStorage():
+    """
+   Storage file created 
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return the dictionary __objects."""
+        """ instance of public function created
+        """
         return FileStorage.__objects
 
     def new(self, obj):
-        """Sets the oibject for the classes"""
-        object_class_name = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(object_class_name, obj.id)] = obj
+        """
+        Creation of relevant files related with assignment
+        """
+        if obj:
+            key_storage = "{}.{}".format(obj.__class__.__name__, obj.id)
+            FileStorage.__objects[key_storage] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        obj_dict = FileStorage.__objects
-        object_dictionary = {obj: obj_dict[obj].to_dict() for obj in obj_dict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(object_dictionary, f)
+        """
+        Dictionary that creates the file.json file
+        """
+        new_dictionary = {}
+        for key_check, value_check in FileStorage.__objects.items():
+            new_dictionary[key_check] = value_check.to_dict().copy()
+        with open(FileStorage.__file_path, mode='w') as my_file:
+            json.dump(new_dictionary, my_file)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """
+       JSON FILE DECENTRALIZED INTO IT
+        """
         try:
-            with open(FileStorage.__file_path) as f:
-                object_dictionary = json.load(f)
-                for o in object_dictionary.values():
-                    class_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(class_name)(**o))
+            with open(FileStorage.__file_path, mode='r') as my_file:
+                new_dictionary = json.load(my_file)
+
+            for key_check, value_check in new_dictionary.items():
+                class_create = value_check.get('__class__')
+                object_create = eval(class_create + '(**value_check)')
+                FileStorage.__objects[key_check] = object_create
+
         except FileNotFoundError:
-            raise FileNotFoundError("File not found: {}".format(FileStorage.__file_path))
-
-
+            pass
 
