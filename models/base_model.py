14473330d6fb50ccfe14265f3aa1a5e628a6a87a
= <=>
2023-08-13 20:53:28 -0700

    fixed

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            models.new_storage.new(self)
 
     def __str__(self):
         """string for base model class"""
@@ -32,7 +32,7 @@ class BaseModel():
     def save(self):
         """modified information regarding time and date"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        models.new_storage.save()
 
     def to_dict(self):
         """delete dictionary  info"""
